33d7ef65e32ba62bc4f49b4d7b7bd9cbbbde8ddb
Mahir7878 <mahirmahir8951@gmail.com>
2024-11-25 23:14:46 +0530

    React Practice Problem

--- react pracitce Problem/src/components/Nineteen.jsx
+++ react pracitce Problem/src/components/Nineteen.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useTheme } from './ThemeContext'
 
 const Nineteen = () => {
+    const {isDarkMode,toggleTheme} = useTheme();
   return (
     <div>
         <label>
-            <input type="checkbox" />
+            <input type="checkbox" checked={isDarkMode} onChange={toggleTheme}/>
             <h2>Dark Mode</h2>
         </label>
     </div>
--- react pracitce Problem/src/components/NineteethComp.jsx
+++ react pracitce Problem/src/components/NineteethComp.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useTheme } from './ThemeContext'
+import './styles/NineTeen.css'
 
 export default function NineteethComp() {
+
+    const {isDarkMode} = useTheme()
+    const themeClass = isDarkMode ? 'dark-theme' : 'light-theme'
   return (
-    <div>NineteethComp</div>
+    <div className={`container ${themeClass}`}>
+        <p>This component uses selected theme</p>
+    </div>
   )
 }
