eeb83e69c5132360063c1de00e8586a979dde4db
Mahir7878 <mahirmahir8951@gmail.com>
2025-01-13 23:42:02 +0530

    React Query

--- react Query/src/main.jsx
+++ react Query/src/main.jsx
@@ -3,8 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+      path: '/',
+      element: <App />,
+  },
+  {
+      path: '/products',
+      element: <Products />,
+  },
+  {
+      path: '/products/:productId',
+      element: <Product />,
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
