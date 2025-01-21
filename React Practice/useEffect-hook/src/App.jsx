f1b328e2a5ad33a83d58c259039436a22d37ea4b
Mahir7878 <mahirmahir8951@gmail.com>
2025-01-21 22:44:39 +0530

    React Practice

--- React Practice/useEffect-hook/src/App.jsx
+++ React Practice/useEffect-hook/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   // const [count, setCount] = useState(0)
   // const [total, setTotal] = useState(1)
 
+  
   // useEffect
   // first --> side-effect function
   // second --> clean-up function
